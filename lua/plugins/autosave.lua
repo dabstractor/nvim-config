65b66d7fe7fe185239f954ffc26cdf46e2e32d4e
Dustin Schultz <dustindschultz@gmail.com>
2026-07-04 01:12:57 -0400

    add autosave runtime toggle controls
    Refines the autosave condition to strictly check for boolean `true` to avoid truthiness pitfalls. Introduces user commands and a `<leader>ua` keymap to enable, disable, or toggle autosave at runtime with immediate status feedback.

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -3,8 +3,32 @@ return {
   config = function()
     require('auto-save').setup {
       condition = function(buf)
-        return vim.g.autosave_enabled
+        -- Gate every autosave on the runtime flag. Compare to `true` explicitly:
+        -- the flag is a boolean (seeded from NVIM_AUTOSAVE in user/config.lua),
+        -- and this also dodges Lua's "0 is truthy" trap if it's ever a number.
+        return vim.g.autosave_enabled == true
       end,
     }
+
+    local function report()
+      vim.notify('Autosave ' .. (vim.g.autosave_enabled and 'ON' or 'OFF'), vim.log.levels.INFO)
+    end
+
+    vim.api.nvim_create_user_command('AutosaveToggle', function()
+      vim.g.autosave_enabled = not (vim.g.autosave_enabled == true)
+      report()
+    end, { desc = 'Toggle autosave on/off' })
+
+    vim.api.nvim_create_user_command('AutosaveOn', function()
+      vim.g.autosave_enabled = true
+      report()
+    end, { desc = 'Enable autosave' })
+
+    vim.api.nvim_create_user_command('AutosaveOff', function()
+      vim.g.autosave_enabled = false
+      report()
+    end, { desc = 'Disable autosave' })
+
+    vim.keymap.set('n', '<leader>ua', '<cmd>AutosaveToggle<cr>', { desc = 'Toggle [a]utosave' })
   end,
 }
